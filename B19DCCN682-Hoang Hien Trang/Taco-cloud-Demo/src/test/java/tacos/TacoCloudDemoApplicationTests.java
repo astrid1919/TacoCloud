b3914ad79ca604068877768b4c9eed0604dbcc0c
astrid1919 <105370844+astrid1919@users.noreply.github.com>
2022-05-11 21:18:58 +0700

    Add files via upload

---
+++ B19DCCN682-Hoang Hien Trang/Taco-cloud-Demo/src/test/java/tacos/TacoCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ B19DCCN682-Hoang Hien Trang/taco-cloud/src/main/java/tacos/data/IngredientRepository.java
@@ -0,0 +1,5 @@
+package tacos.data;
+import tacos.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+} 
